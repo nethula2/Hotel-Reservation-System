46755a544d273577b23c595c180614834d2438ab
nethula2 <sanvidunethula2@gmail.com>
2026-05-16 22:19:00 +0530

    Add Hotel and Room DAOs for Reservation Management

--- src/main/java/com/hotelmanagement/system/dao/RoomDAO.java
+++ src/main/java/com/hotelmanagement/system/dao/RoomDAO.java
@@ -1,64 +1,60 @@
 package com.hotelmanagement.system.dao;
 
 import com.hotelmanagement.system.model.Room;
-import com.hotelmanagement.system.utils.DBConnection;
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RoomDAO {
+import static com.hotelmanagement.system.util.DBConnection.getConnection;
 
+public class RoomDAO {
     public List<Room> getRoomsByHotelId(int hotelId) throws SQLException {
         List<Room> rooms = new ArrayList<>();
-        String query = "SELECT * FROM rooms WHERE hotel_id = ?";
-        try (Connection conn = DBConnection.getConnection();
-             PreparedStatement ps = conn.prepareStatement(query)) {
-            ps.setInt(1, hotelId);
-            try (ResultSet rs = ps.executeQuery()) {
-                while (rs.next()) {
-                    rooms.add(mapResultSetToRoom(rs));
-                }
-            }
+        Connection conn = getConnection();
+        if (conn == null) return rooms;
+
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM rooms WHERE hotel_id = ?");
+        stmt.setInt(1, hotelId);
+        ResultSet rs = stmt.executeQuery();
+
+        while (rs.next()) {
+            Room room = new Room();
+            room.setId(rs.getInt("id"));
+            room.setHotelId(rs.getInt("hotel_id"));
+            room.setRoomType(rs.getString("room_type"));
+            room.setPricePerNight(rs.getDouble("price_per_night"));
+            room.setCapacity(rs.getInt("capacity"));
+            room.setTotalRooms(rs.getInt("total_rooms"));
+            room.setAvailableRooms(rs.getInt("available_rooms"));
+            room.setDescription(rs.getString("description"));
+            rooms.add(room);
         }
+        conn.close();
         return rooms;
     }
 
     public Room getRoomById(int roomId) throws SQLException {
-        String query = "SELECT * FROM rooms WHERE id = ?";
-        try (Connection conn = DBConnection.getConnection();
-             PreparedStatement ps = conn.prepareStatement(query)) {
-            ps.setInt(1, roomId);
-            try (ResultSet rs = ps.executeQuery()) {
-                if (rs.next()) {
-                    return mapResultSetToRoom(rs);
-                }
-            }
+        Connection conn = getConnection();
+        if (conn == null) return null;
+
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM rooms WHERE id = ?");
+        stmt.setInt(1, roomId);
+        ResultSet rs = stmt.executeQuery();
+
+        if (rs.next()) {
+            Room room = new Room();
+            room.setId(rs.getInt("id"));
+            room.setHotelId(rs.getInt("hotel_id"));
+            room.setRoomType(rs.getString("room_type"));
+            room.setPricePerNight(rs.getDouble("price_per_night"));
+            room.setCapacity(rs.getInt("capacity"));
+            room.setTotalRooms(rs.getInt("total_rooms"));
+            room.setAvailableRooms(rs.getInt("available_rooms"));
+            room.setDescription(rs.getString("description"));
+            conn.close();
+            return room;
         }
+        conn.close();
         return null;
-    }
-
-    public boolean updateRoomAvailability(int roomId, int change) throws SQLException {
-        String query = "UPDATE rooms SET available_rooms = available_rooms + ? WHERE id = ? AND available_rooms + ? >= 0";
-        try (Connection conn = DBConnection.getConnection();
-             PreparedStatement ps = conn.prepareStatement(query)) {
-            ps.setInt(1, change);
-            ps.setInt(2, roomId);
-            ps.setInt(3, change);
-            return ps.executeUpdate() > 0;
-        }
-    }
-
-    private Room mapResultSetToRoom(ResultSet rs) throws SQLException {
-        Room room = new Room();
-        room.setId(rs.getInt("id"));
-        room.setHotelId(rs.getInt("hotel_id"));
-        room.setRoomType(rs.getString("room_type"));
-        room.setPricePerNight(rs.getDouble("price_per_night"));
-        room.setCapacity(rs.getInt("capacity"));
-        room.setTotalRooms(rs.getInt("total_rooms"));
-        room.setAvailableRooms(rs.getInt("available_rooms"));
-        room.setDescription(rs.getString("description"));
-        return room;
     }
 }
