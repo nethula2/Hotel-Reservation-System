1b2da69608600381a484dcbbd7bfe1025764c285
sithupa Vismal Wijewardhana <it25102342@my.sliit.lk>
2026-05-18 20:45:45 +0530

    Added Default constructor to prevent initialize basic user properties.

---
+++ hotel-management-system-v1-main/src/main/java/com/hotelmanagement/system/model/Customer.java
@@ -0,0 +1,19 @@
+package com.hotelmanagement.system.model;
+
+    //parent User class initialize any basic user properties
+    public class Customer extends User{
+        public Customer() {
+            super();
+            setRole("CUSTOMER"); //automatically sets the user role
+        }
+
+    //use to create new customer
+    public Customer(String name, String email, String password, String phone) {
+        super(name, email, password, phone, "CUSTOMER");
+    }
+
+    @Override
+    public String getUserHomePageUrl(){
+        return "/customer/home"; //provide own specific
+    }
+}
